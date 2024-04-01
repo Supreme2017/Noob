88a5c7976c8533a5614261ef2fa4209306a111b1
Zoro <aliahmad3843@gmail.com>
2024-04-01 22:53:45 +0500

    links dead

--- src/index.js
+++ src/index.js
@@ -24,17 +24,22 @@ client.on('ready', () => {
     })
   })
   
-  const contentLowercase = message.content.toLowerCase();
-  //Asim Uncle
-   if (contentLowercase.includes("asim muneer")||contentLowercase.includes("army")||contentLowercase.includes("asim munir")) {
-      message.reply("https://media.tenor.com/Va1kCZB5Ot4AAAAM/cop-police.gif");
-  };
+  
 
 
   // Links delete
   const exemptRoleIDs = ['1212820572724264990', '1223748824699244644', '1210943307111211038','1212431099028971555','1223429887839309844','1190374142403416074','1219633686908239932', '1210962834029158460'];
   const allowedChannelID = '1210955522421817385';
   client.on('messageCreate', message => {
+
+
+    //Asim Uncle
+    const contentLowercase = message.content.toLowerCase();
+   if (contentLowercase.includes("asim muneer")||contentLowercase.includes("army")||contentLowercase.includes("asim munir")) {
+      message.reply("https://media.tenor.com/Va1kCZB5Ot4AAAAM/cop-police.gif");
+  };
+
+  //links shit
 
     if (message.channel.id !== allowedChannelID) {
       return;
