b3384dbbec6a3e0e8f24e5a931e3e491b42ab9d0
Zoro <aliahmad3843@gmail.com>
2024-04-01 16:41:07 +0500

    links dead

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const client = new Client({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMessageReactions,
         IntentsBitField.Flags.MessageContent,
         IntentsBitField.Flags.DirectMessageTyping,
     ]
@@ -24,17 +25,40 @@ client.on('ready', () => {
   })
   
 
-client.on ('messageCreate',(message)=>{
-    if (message.author.bot){
+
+  // Links delete
+  const exemptRoleIDs = ['1212820572724264990', '1223748824699244644', '1210943307111211038','1212431099028971555','1223429887839309844','1190374142403416074','1219633686908239932'];
+  const allowedChannelID = '1210955522421817385';
+  client.on('messageCreate', message => {
+
+    if (message.channel.id !== allowedChannelID) {
+      return;
+  }
+
+    if (message.author.bot || exemptRoleIDs.some(roleID => message.member.roles.cache.has(roleID))) {
         return;
     }
-    const contentLowercase = message.content.toLowerCase();
-    //Asim Uncle
-     if (contentLowercase.includes("asim muneer")||contentLowercase.includes("army")||contentLowercase.includes("asim munir")) {
-        message.reply("https://media.tenor.com/Va1kCZB5Ot4AAAAM/cop-police.gif");
-    };
 
-})
+    if (message.content.match(/https?:\/\/\S+/)) {
+        message.delete()
+            .then(deletedMessage => {
+                console.log(`Deleted message from ${deletedMessage.author.username}: ${deletedMessage.content}`);
+                // Send a message mentioning the user
+                message.channel.send(`<@${deletedMessage.author.id}>, You're not allowed to send links here.`)
+                    .then(sentMessage => {
+                        // Delete the message after 4 seconds
+                        setTimeout(() => {
+                            sentMessage.delete().catch(error => console.error('Error deleting message:', error));
+                        }, 4000);
+                    })
+                    .catch(error => console.error('Error sending message:', error));
+            })
+            .catch(error => console.error('Error deleting message:', error));
+    }
+});
+
+
+
 
 // welcome command
 const welcomeChannelID="1210955522421817385"
@@ -45,6 +69,26 @@ client.on('guildMemberAdd', member => {
   if (!channel) return;
   channel.send(`Welcome to the Supreme Family, ${member}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //slash commands
