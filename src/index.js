fc954954e973a1b8e09915e43384b5ac29a0b190
Zoro <aliahmad3843@gmail.com>
2024-04-03 15:29:27 +0500

    mods role added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { inspect, } = require ('util');
-const{Client,IntentsBitField, EmbedBuilder, Routes, DataResolver, ActivityType, Emoji, Embed, ReactionEmoji, GuildEmoji}=require('discord.js')
+const{Client,IntentsBitField, WebhookClient, EmbedBuilder, Routes, DataResolver, ActivityType, Emoji, Embed, ReactionEmoji, GuildEmoji}=require('discord.js')
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -26,9 +26,8 @@ client.on('ready', () => {
   
   
 
-
   // Links delete
-  const exemptRoleIDs = ['1212820572724264990', '1223748824699244644', '1210943307111211038','1212431099028971555','1223429887839309844','1190374142403416074','1219633686908239932', '1210962834029158460'];
+  const exemptRoleIDs = ['1212820572724264990', '1223748824699244644', '1210943307111211038','1212431099028971555','1223429887839309844','1190374142403416074','1219633686908239932', '1210962834029158460','1224673398840365157','1224673501613395969','1224673745524887572','1224673991244120104','1224674246446288917','1224674491049709588','1224791423430299678'];
   const allowedChannelID = '1210955522421817385';
   client.on('messageCreate', message => {
 
@@ -70,6 +69,28 @@ client.on('ready', () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // welcome command
 const welcomeChannelID="1210955522421817385"
 const guildID="1189591582375166114"
@@ -79,22 +100,6 @@ client.on('guildMemberAdd', member => {
   if (!channel) return;
   channel.send(`Welcome to the Supreme Family, ${member}!`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -144,3 +149,99 @@ client.on('interactionCreate', (interaction) => {
 
 
 client.login(process.env.key);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//RUlesssssssssssss
+// // Event for when a message is received
+// client.on('messageCreate', message => {
+//   // Check if the message starts with !rules
+//   if (message.content.startsWith('!rules')) {
+//       // Create an embed for general rules
+//       const generalEmbed = {
+//           title: 'Server Rules',
+//           description: 'Please adhere to the following rules:',
+//           color: parseInt('ff0000', 16), // Convert hexadecimal color code to integer
+//           fields: [
+//               { name: '1. Follow Discord Policies', value: 'This rule must be followed at all times.' },
+//               { name: '2. Be Respectful', value: 'Be respectful to someone who you don’t know. If the person complains, you might receive a penalty.' },
+//               { name: '3. No Sexism', value: 'Usage of excessive extreme racial/sexist slurs is prohibited and will result in mute or ban.' },
+//               { name: '4. No Pedophilia', value: 'This includes pedophilia grooming jokes, remarks, references, pictures, etc.' },
+//               { name: '5. No Gore/NSF', value: 'No gore or NSFW content allowed, including your profile picture, status, and name.' },
+//               { name: '6. No Leaking', value: 'No doxing or posting someone\'s personal information or photos without their consent.' },
+//               { name: '7. No Malicious Links', value: 'Sending or linking any harmful material such as viruses, IP grabbers, or phishing links will result in an immediate and permanent ban.' },
+//               { name: '8. No Advertising', value: 'Posting invite links or self-promoting is not allowed. Promotion must be approved by Mods.' },
+//               { name: '9. No Religious Arguments', value: 'Respect others\' beliefs and avoid religious arguments.' }
+//           ]
+//       };
+
+//       // Create an embed for the last rule
+//       const lastRuleEmbed = {
+//           title: 'Complaints or Questions',
+//           description: 'If you have any complaints or questions, you can contact the moderators.',
+//           color: parseInt('0000ff', 16) // Convert hexadecimal color code to integer
+//       };
+
+//       // Send the general rules embed to the channel
+//       message.channel.send({ embeds: [generalEmbed] });
+
+//       // Send the last rule embed to the channel
+//       message.channel.send({ embeds: [lastRuleEmbed] });
+//   }
+// });
+
+// // Event for when a message is received
+// client.on('messageCreate', message => {
+//   // Check if the message starts with !levels
+//   if (message.content.startsWith('!levels')) {
+//       // Create an embed using the embed builder syntax
+//       const embed = {
+//           title: 'Perks',
+//           description: 'Here are the perks you can get by staying active in the chat:',
+//           color: 0x00FF00, // Green color
+//           fields: [
+//               { name: 'Level 10', value: '- External emojis + stickers' },
+//               { name: 'Level 20', value: '- Slash Commands' }, // Swapped with Level 20
+//               { name: 'Level 30', value: '- Attach Files + Message Threads + Activities' }, // Swapped with Level 30
+//               { name: 'Level 40', value: '- Manage Nicknames + Soundboard' },
+//               { name: 'Level 50', value: '- Send Links' },
+//               { name: 'Level 60', value: '- Timeout perms + Voice messages' },
+//               { name: 'Level 70', value: '- Visible role + Private Channels access' },
+//               { name: 'Level 80', value: '- Visible role + Special Role Icon + Kick/Timeout members' },
+//               { name: 'Level 90', value: '- Visible role + Role Icon + Moderator' },
+//               { name: 'Level 100', value: '- Visible role + Role icon + Admin Power' }
+//           ]
+//       };
+
+//       // Send the embed to the channel
+//       message.channel.send({ embeds: [embed] });
+//   }
+// });
+
